2c0c05f9089aad836bdcbec9af0bffb4873ef3e3
Shubham <shubhamy@iitbhilai.ac.in>
2023-08-22 12:57:29 +0530

    courses with undefined slots incorporated as a message in pdf

--- backend.py
+++ backend.py
@@ -95,8 +95,7 @@ def fetch_db_data(db_filepath, user_input):
              31: "E2", 32: "A2", 33: "D2", 34: "F1", 35: "G2", 36: "K2", 37: "M2", 38: "I2",
              41: "F2", 42: "B3", 43: "C3", 44: "E3", 45: "H2", 46: "L3", 47: "M3", 48: "J2",
              51: "G3", 52: "A3", 53: "D3", 54: "F3", 55: "H3", 56: "K3", 57: "J3", 58: "I3",
-
-             1: "N", 2: "O", 3: "V", 4: "W", 5: "P", 6: "Q", 7: "R", 8: "S", 9: "T", 10: "U"}
+             0:[]}
     # Value of index dictionary changes every time program is run
 
     permanent_index = {11: "A1", 12: "B1", 13: "C1", 14: "E1", 15: "G1", 16: "K1", 17: "L1", 18: "I1",
@@ -104,15 +103,15 @@ def fetch_db_data(db_filepath, user_input):
              31: "E2", 32: "A2", 33: "D2", 34: "F1", 35: "G2", 36: "K2", 37: "M2", 38: "I2",
              41: "F2", 42: "B3", 43: "C3", 44: "E3", 45: "H2", 46: "L3", 47: "M3", 48: "J2",
              51: "G3", 52: "A3", 53: "D3", 54: "F3", 55: "H3", 56: "K3", 57: "J3", 58: "I3",
-
-             1: "N", 2: "O", 3: "V", 4: "W", 5: "P", 6: "Q", 7: "R", 8: "S", 9: "T", 10: "U"}
+             0:[]}
 
     Slot_key = {"A1": 11, "B1": 12, "C1": 13, "E1": 14, "G1": 15, "K1": 16, "L1": 17, "I1": 18,
                 "B2": 21, "D1": 22, "C2": 23, "BLANK": 24,"H1": 25, "L2": 26, "J1": 27, "M1": 28,
                 "E2": 31, "A2": 32, "D2": 33, "F1": 34, "G2": 35, "K2": 36, "M2": 37, "I2": 38,
                 "F2": 41, "B3": 42, "C3": 43, "E3": 44, "H2": 45, "L3": 46, "M3": 47, "J2": 48,
                 "G3": 51, "A3": 52, "D3": 53, "F3": 54, "H3": 55, "K3": 56, "J3": 57, "I3": 58,
-                "N": 1, "O": 2, "V": 3, "W": 4, "P": 5, "Q": 6, "R": 7, "S": 8, "T": 9, "U": 10}
+
+                "MSG":0}
     connection = sqlite3.connect(db_filepath)
     cursor = connection.cursor()
     keys = []
@@ -128,8 +127,9 @@ def fetch_db_data(db_filepath, user_input):
 
     connection.close()
 
-    pattern1 = re.compile("[A-Z][0-9]{2}")  # for D12 vaghera
-    pattern2 = re.compile("[A-Z][0-9]{1}")  # for D1
+    pattern1 = re.compile("[A-Z]{1}[0-9]{2}")  # for D12 vaghera
+    pattern2 = re.compile("[A-Z]{1}[0-9]{1}")  # for D1
+
 
     for course_slot in slots:
 
@@ -139,9 +139,9 @@ def fetch_db_data(db_filepath, user_input):
             slot = slot.strip()
 
             if re.findall(pattern1, slot) == []:
-                if re.findall(pattern2, slot) == []:  # single alphabet
+                if re.findall(pattern2, slot) == []:  # single alphabet or (LAB/TBA/NA (ie text string))
 
-                    alphabet = slot[0]
+                    alphabet = slot
                     #( "N" or "O" or "V" or "W" or "P" or "Q" or "R" or "S" or "T" or "U")
                     match alphabet:
                         case "N":
@@ -185,10 +185,15 @@ def fetch_db_data(db_filepath, user_input):
                             add.append(Slot_key[f"J3"])
                             add.append(Slot_key[f"I3"])
 
-                        case _: # NOT LAB SLOTS
+                        case "A"|"B"|"C"|"D"|"E"|"F"|"G"|"H"|"I"|"J"|"K"|"L"|"M" :
                             add.append(Slot_key[f"{alphabet}1"])
                             add.append(Slot_key[f"{alphabet}2"])
                             add.append(Slot_key[f"{alphabet}3"])
+
+
+                        case _: # NON DEFINED SLOTS
+                            add.append(Slot_key["MSG"])
+
 
                 else:
                     add.append(Slot_key[slot])
@@ -201,29 +206,27 @@ def fetch_db_data(db_filepath, user_input):
                 add.append(Slot_key[f"{alphabet}{dig_2}"])
         keys.append(add)
 
-    """i = 0
-    for lst in keys:
-        for key in lst:
-            index[key] = user_input[i]
 
-        i = i + 1
-
-    return index"""
-    content=[]
     clashes=[]
+    additional_messages = []  # to indicate lab / na courses
     pattern = re.compile("[0-9]{3}")  # course codes have 3 digits in the end
     i = 0
     for lst in keys:
         for key in lst:
-            if re.findall(pattern, index[key]) != []: # clash has occured course is already present in that section
-                clashes.append(f"Course {user_input[i]} is clashing with {index[key]} in the {permanent_index[key]} slot ")
+            if key==0: # NA slots
+                index[key].append(user_input[i])
 
             else:
-                index[key] = user_input[i]
+                if re.findall(pattern, index[key]) != []: # clash has occured course is already present in that section
+                    clashes.append(f"Course {user_input[i]} is clashing with {index[key]} in the {permanent_index[key]} slot ")
+
+                else:
+                    index[key] = user_input[i]
 
         i = i + 1
-
-    content=[index,clashes]
+    for NA_courses in index[0]:
+        additional_messages.append(f"Course {NA_courses} has no slot defined")
+    content=[index,clashes,additional_messages]
     return content
 
 def fetch_course_codes_db(db_filepath):
@@ -266,6 +269,7 @@ def fetch_course_info_db(db_filepath,code):
 def generate_pdf(content):
     index=content[0]
     clashes=content[1]
+    additional_messages=content[2]
     Day = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thrusday", 5: "Friday"}
     """ LOADING DATA IN PDF """
 
@@ -304,6 +308,15 @@ def generate_pdf(content):
                     pdf.cell(w=20, h=8, txt=index[(i * 10 + j)], border=1,fill=True,ln=1) # coloring cells which have courses
                 else:
                     pdf.cell(w=20, h=8, txt=index[(i * 10 + j)], border=1, fill=False,ln=1)
+
+    # adding message that course is LAB course / NA /TBA
+    pdf.ln(50)
+    pdf.set_font(family="Times", size=9)
+
+    for message in additional_messages:
+        pdf.cell(w=100, h=8, txt=message, ln=1)
+
+
 
     # adding clash messages
     pdf.ln(80)
