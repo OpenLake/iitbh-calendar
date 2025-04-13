8cbc6d3ebb0ba5c03b5bc56ebe72b062148167d5
Shubham <shubhamy@iitbhilai.ac.in>
2025-04-13 13:30:09 +0530

    DRF version initiated

--- TimeTable_Generator/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('view-pdf/', views.pdf_view,name='pdf_view'),
-
-
-]
--- backend/mysite/urls.py
+++ backend/mysite/urls.py
@@ -2,7 +2,7 @@
 URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('TimeTable_Generator.url')),
-
 ]
