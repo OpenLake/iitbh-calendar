cab207ff3b9cde0c97ff6809d86ea5772362d14e
ashutosh229 <akumarjha875@gmail.com>
2025-04-15 21:48:49 +0530

    configured the redux store

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+    FLUSH,
+    PAUSE,
+    PERSIST,
+    persistReducer,
+    persistStore,
+    PURGE,
+    REGISTER,
+    REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+const rootReducer = combineReducers({
+
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
