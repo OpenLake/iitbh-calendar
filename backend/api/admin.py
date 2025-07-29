1b709547fb864d7dfdfcd921e5ad5f356671010f
Shubham <shubhamy@iitbhilai.ac.in>
2025-07-29 13:34:40 +0530

    admin panel modified

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CourseData
+
+class CourseDataAdmin(admin.ModelAdmin):
+    list_display = ("course_code","course_name", "slot")
+    list_filter = ("discipline",)
+    search_fields = ("course_code","course_name")
+
+admin.site.register(CourseData,CourseDataAdmin)
